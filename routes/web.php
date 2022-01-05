27bc87cf4ca08902a4a9d193ba2a913aa7a1b244
rezwansaki <rezwansaki@gmail.com>
2022-01-06 00:08:54 +0600

    model created

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
+use App\Models\Blog;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/getdata', function () {
+    $blogs = Blog::all();
+    return $blogs;
+});
+
 
 
 Route::resource('post', App\Http\Controllers\PostController::class);
