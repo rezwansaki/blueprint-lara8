0c4c732cc346ef12aa9281ff938e04c6977567d0
rezwansaki <rezwansaki@gmail.com>
2024-04-28 20:02:18 +0600

    add SQLite database with this project also make auth

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get('/getdata', [BlogController::class, 'index']);
 
 
 Route::resource('post', App\Http\Controllers\PostController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
